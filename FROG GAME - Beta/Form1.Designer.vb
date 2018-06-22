b0b4a8bf69628889b42352300cbec865948149fd
MarkDino21 <markdinopelonia447@gmail.com>
2018-06-22 09:25:20 +0800

    frog ride to lag

--- FROG GAME - Beta/Form1.Designer.vb
+++ FROG GAME - Beta/Form1.Designer.vb
@@ -22,10 +22,52 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.picFrog = New System.Windows.Forms.PictureBox()
+        Me.picLag = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.picFrog, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picLag, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picFrog
+        '
+        Me.picFrog.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.picFrog.Location = New System.Drawing.Point(441, 374)
+        Me.picFrog.Name = "picFrog"
+        Me.picFrog.Size = New System.Drawing.Size(50, 48)
+        Me.picFrog.TabIndex = 0
+        Me.picFrog.TabStop = False
+        '
+        'picLag
+        '
+        Me.picLag.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.picLag.Location = New System.Drawing.Point(569, 274)
+        Me.picLag.Name = "picLag"
+        Me.picLag.Size = New System.Drawing.Size(219, 55)
+        Me.picLag.TabIndex = 1
+        Me.picLag.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.picFrog)
+        Me.Controls.Add(Me.picLag)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.picFrog, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picLag, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents picFrog As PictureBox
+    Friend WithEvents picLag As PictureBox
+    Friend WithEvents Timer1 As Timer
 End Class
--- FROG GAME - Beta/Form1.vb
+++ FROG GAME - Beta/Form1.vb
@@ -1,3 +1,36 @@
 ﻿Public Class Form1
+    Dim frogMove As Boolean
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Up Then
+            picFrog.Top -= 10
+
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        picLag.Left -= 5
+
+        If frogMove = True Then
+            picFrog.Left -= 5
+
+        End If
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
+        If e.KeyCode = Keys.Up Then
+            If picFrog.Left >= picLag.Left And picFrog.Left <= picLag.Left + picLag.Width - picFrog.Width And picFrog.Top >= picLag.Top And
+            picFrog.Top <= picLag.Top + picLag.Height - picFrog.Height Then
+                frogMove = True
+            Else
+                frogMove = False
+
+            End If
+        End If
+
+    End Sub
 End Class
