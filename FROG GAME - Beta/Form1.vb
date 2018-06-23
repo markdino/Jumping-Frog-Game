4352d8c9d0e94c89d5f7502ae45aabc5d1047b07
MarkDino21 <markdinopelonia447@gmail.com>
2018-06-23 09:27:05 +0800

    frog ride to lag change from keyUp to timer

--- FROG GAME - Beta/Form1.vb
+++ FROG GAME - Beta/Form1.vb
@@ -11,14 +11,14 @@
     End Sub
     Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
         If e.KeyCode = Keys.Up Or e.KeyCode = Keys.Down Or e.KeyCode = Keys.Left Or e.KeyCode = Keys.Right Then
-            If picFrog.Left >= picLog.Left And picFrog.Left <= picLog.Left + picLog.Width - picFrog.Width And picFrog.Top >= picLog.Top And
-            picFrog.Top <= picLog.Top + picLog.Height - picFrog.Height Then
-                frogMove = True
-                Label1.Text = "Inside"
-            Else
-                frogMove = False
-                Label1.Text = "Outside"
-            End If
+            'If picFrog.Left >= picLog.Left And picFrog.Left <= picLog.Left + picLog.Width - picFrog.Width And picFrog.Top >= picLog.Top And
+            'picFrog.Top <= picLog.Top + picLog.Height - picFrog.Height Then
+            '    frogMove = True
+            '    Label1.Text = "Inside"
+            'Else
+            '    frogMove = False
+            '    Label1.Text = "Outside"
+            'End If
         End If
 
     End Sub
@@ -26,6 +26,16 @@
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         'picLog.Left -= 5
         Car_moveLeft(sender, e)
+
+        If picFrog.Left >= picLog.Left And picFrog.Left <= picLog.Left + picLog.Width - picFrog.Width And picFrog.Top >= picLog.Top And
+            picFrog.Top <= picLog.Top + picLog.Height - picFrog.Height Then
+            frogMove = True
+            Label1.Text = "Inside"
+        Else
+            frogMove = False
+            Label1.Text = "Outside"
+        End If
+
         If frogMove = True Then
             picFrog.Left -= 5
         Else
