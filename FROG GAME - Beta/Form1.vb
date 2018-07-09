ee67cb2642eaaf2f47b7575b709f6e841e534c76
MarkDino21 <markdinopelonia447@gmail.com>
2018-07-09 18:51:13 +0800

    stopping frog from passing through the bottom edge

--- FROG GAME - Beta/Form1.vb
+++ FROG GAME - Beta/Form1.vb
@@ -328,15 +328,17 @@ picFrog.Top <= picLag12.Top + picLag12.Height Then
             picFrog.Top -= 20
             picFrog.Image = My.Resources.frog1
         ElseIf e.KeyCode = Keys.Down Then
-            picFrog.Top += 20
             picFrog.Image = My.Resources.frog1Down
-        ElseIf e.KeyCode = Keys.Left Then
-            picFrog.Image = My.Resources.frog1Left
-            If picFrog.Left - 5 > 0 Then
-                picFrog.Left -= 20
+            If picFrog.Top + picFrog.Height < GroupBox1.Height Then
+                picFrog.Top += 20
             End If
-        ElseIf e.KeyCode = Keys.Right Then
-            picFrog.Image = My.Resources.frog1Right
+        ElseIf e.KeyCode = Keys.Left Then
+                picFrog.Image = My.Resources.frog1Left
+                If picFrog.Left - 5 > 0 Then
+                    picFrog.Left -= 20
+                End If
+            ElseIf e.KeyCode = Keys.Right Then
+                picFrog.Image = My.Resources.frog1Right
             If picFrog.Left + picFrog.Width + 8 < picRiver.Width Then
                 picFrog.Left += 20
             End If
