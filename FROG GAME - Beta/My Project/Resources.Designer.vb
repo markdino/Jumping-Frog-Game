d491b0a936e8f70b9405e47d29a8fc82325efb4b
Mark Dino <markdinopelonia447@gmail.com>
2019-01-31 00:04:32 +0800

    feat: Add Splash Screen

--- FROG GAME - Beta/My Project/Resources.Designer.vb
+++ FROG GAME - Beta/My Project/Resources.Designer.vb
@@ -357,6 +357,16 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property splashScreen() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("splashScreen", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         '''</summary>
         Friend ReadOnly Property splat() As System.IO.UnmanagedMemoryStream
---
+++ FROG GAME - Beta/frmSplashScreen.vb
@@ -0,0 +1,29 @@
+﻿Public Class frmSplashScreen
+    Private Sub frmSplashScreen_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Dim intnum As Integer
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        intnum += 1
+        If lblProgress.Text = "Loading . . ." Then
+            lblProgress.Text = "Loading ."
+        ElseIf lblProgress.Text = "Loading ." Then
+            lblProgress.Text = "Loading . ."
+        ElseIf lblProgress.Text = "Loading . ." Then
+            lblProgress.Text = "Loading . . ."
+        End If
+
+        If intnum < 100 Then
+            lblPercent.Text = intnum & "%"
+            ProgressBar1.Value = intnum
+        ElseIf intnum = 100 Then
+            lblPercent.Text = "100%"
+            lblProgress.Text = "Finished"
+            ProgressBar1.Value = intnum
+        ElseIf intnum = 110 Then
+            Timer1.Enabled = False
+            frmSplash.Show()
+            Close()
+        End If
+    End Sub
+End Class
